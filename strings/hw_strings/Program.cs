625739de7e0159084f5c6fab58fa3f0e04be5655
NijatMammadli <nijatfm@code.edu.az>
2020-12-26 21:27:09 +0400

    strings

---
+++ strings/hw_strings/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace hw_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Start_a:
+            Console.WriteLine("insert a number with 3 digits:");
+
+            string a = Console.ReadLine();
+            if (a.Length != 3)
+            {
+                goto Start_a;
+            }
+
+
+        Start_b:
+            Console.WriteLine("insert a number with 5 digits:");
+
+            string b = Console.ReadLine();
+            if (b.Length != 5)
+            {
+                goto Start_b;
+            }
+
+            string c = (a.Substring(0,2) + b.Substring(1,4));
+            int number_int = Convert.ToInt32(c);
+            string number_st = Convert.ToString(number_int);
+
+            int first_3 = Convert.ToInt32(number_st.Substring(0, 3));
+            int last_3 = Convert.ToInt32(number_st.Substring(3, 3));
+            int result = last_3 - first_3;
+
+            if(result < 0)
+            {
+                Console.WriteLine("difference is negative:");
+                goto Start_a;
+            }
+            Console.WriteLine($"result is { result} \t");
+            
+            
+
+        }
+    }
+}
---
+++ strings/hw_strings_2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace hw_strings_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        START:
+
+            Console.WriteLine("insert a word which suits to conditions");
+            string word = Console.ReadLine();
+            int b_n = word.ToLower().IndexOf('b');
+            
+            if (word.Length != 6)
+            {
+                Console.WriteLine("word should be 5 letters");
+                goto START;
+            }
+            else if (word.ToLower().Contains('a'))
+            {
+                Console.WriteLine("word should not contain a");
+                goto START;
+            }
+            else if (word[5] != '.')
+            {
+                Console.WriteLine("at the end of word . sign should be written");
+                goto START;
+            }
+            else if(b_n == -1)
+            {
+                Console.WriteLine("word should containt at least one 'b' character");
+                goto START;
+            }
+           
+            word = word.Replace('u', 'k');
+            
+            word = word.Replace('Z', 'z');
+            Console.WriteLine(Convert.ToInt32(word.Contains('z')));
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (Convert.ToInt32(word.Contains('z')) != -0)
+                {
+                    word = word.Remove(Convert.ToInt32(word.IndexOf('z')), 1);
+                }
+                Console.WriteLine(word);
+            }
+
+            word = word.Replace('o', 'O');
+            word = word.Replace('L', 'l');
+
+            Console.WriteLine(word);
+
+            Console.WriteLine((word.Substring(2, word.Length - 3) + word.Substring(0, 2)).ToLower());
+
+            
+        }
+    }
+}
